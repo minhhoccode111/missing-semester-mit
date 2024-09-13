ec9c5250ba12d1ec3bf30a6efe63ae4d8d682f28
minhhoccode111 <minhhoccode111@gmail.com>
2024-09-13 22:47:28 +0700

    add: finish data wrangling video examples

--- data_wrangling/example.sh
+++ data_wrangling/example.sh
@@ -72,4 +72,33 @@ cat ssh.log | sed -E 's/^.*?Disconnected from (invalid |authenticating )?user (.
 # $2 ~ /^c.*e$/: column 2 match regex pattern
 # {print $0}: print the whole line
 
+# use awk to count line
+cat ssh.log | sed -E 's/^.*?Disconnected from (invalid |authenticating )?user (.*) [0-9.]+ port [0-9]+( \[preauth\])?$/\2/' | sort | uniq -c | awk 'BEGIN {rows = 0} $1 == 1 && $2 ~ /^c.*e$/ {rows += 1} END {print rows}'
+# BEGIN {rows = 0}: before the first line define variabel rows = 0
+# {rows += 1}: when pattern match, increase rows by 1
+# END {print rows}: after the last line print rows
 
+# bakerley calculator
+echo "1 + 2" | bc -l
+# bc -l: calculate the stdin
+
+# sum all the user name attemp more than 1
+cat ssh.log | sed -E 's/^.*?Disconnected from (invalid |authenticating )?user (.*) [0-9.]+ port [0-9]+( \[preauth\])?$/\2/' | sort | uniq -c | awk '$1 != 1 {print $1}' | paste -sd+ | bc -l
+# awk '$1 != 1 {print $1}': print the first column if not 1
+# paste -sd+: paste to one single (-s) line with delimiter '+' (d+)
+# bc -l: calculate the stdin line
+
+# use R programming language for statistic
+cat ssh.log | sed -E 's/^.*?Disconnected from (invalid |authenticating )?user (.*) [0-9.]+ port [0-9]+( \[preauth\])?$/\2/' | sort | uniq -c | awk '{print $1}' | R --slave -e 'x <- scan(file="stdin", quiet=TRUE); summary(x)'
+
+# gnuplot: is a plotter that lets you take things from stdin
+cat ssh.log | sed -E 's/^.*?Disconnected from (invalid |authenticating )?user (.*) [0-9.]+ port [0-9]+( \[preauth\])?$/\2/' | sort | uniq -c | sort -nk1,1 | tail -n5 | gnuplot -p -e 'set boxwith 0.5; plot "-" using 1:xtic(2) with boxes'
+
+# example uninstall not used rust 
+rustup toolchain list | grep nightly | grep -v 'nightly-x86' | grep 2019 | sed 's/-x86.*//' | xargs rustup toolchain uninstall
+
+# example send image to server
+ffmpeg -loglevel panic -i /dev/video0 -frames 1 -f image2 - | convert - -colorspace gray - | gzip | ssh tsp 'gzip -d | tee copy.png' | feh -
+
+# 
+cat /dev/video0 | ssh
